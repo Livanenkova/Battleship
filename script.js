92d4a16aafed055223533e24a6996b0a38bfbeff
Livanenkova <arina909@yandex.ru>
2020-03-18 11:47:53 +0300

    change js script

--- script.js
+++ script.js
@@ -3,14 +3,20 @@ var view = {
  var messageArea = document.getElementById("messageArea");
  messageArea.innerHTML = msg;
  },
-
  displayHit: function(location) {
-  var cell = document.getElementById (location)
-  cell.setAttribute("class", "hit");
+ var cell = document.getElementById(location);
+ cell.setAttribute("class", "hit");
  },
-
  displayMiss: function(location) {
-
-
+ var cell = document.getElementById(location);
+ cell.setAttribute("class", "miss");
  }
 };
+
+view.displayMiss("11");
+view.displayHit("32");
+view.displayMiss("53");
+view.displayHit("15");
+view.displayMiss("25");
+view.displayHit("23");
+view.displayMessage("Tap tap, is this thing on?");
