f5cd83175596d8f4b9a89f066ffb3f8008672fbb
Livanenkova <arina909@yandex.ru>
2020-03-22 22:02:13 +0300

    add isSunk

--- script.js
+++ script.js
@@ -16,11 +16,35 @@ var view = {
 var model = {
  boardSize: 7,
  numShips: 3,
- shipLength: 3,
  shipsSunk: 0,
- ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+ shipLength: 3,
+ ships: [ { locations: ["06", "16", "26"], hits: ["", "", ""] },
  { locations: ["24", "34", "44"], hits: ["", "", ""] },
- { locations: ["10", "11", "12"], hits: ["", "", ""] }]
+ { locations: ["10", "11", "12"], hits: ["", "", ""] } ],
+ fire: function(guess) {
+ for (var i = 0; i < this.numShips; i++) {
+ var ship = this.ships[i];
+ var locations = ship.locations;
+ var index = locations.indexOf(guess);
+ if (index >= 0) {
+ ship.hits[index] = "hit";
+ if (this.isSunk(ship)) {
+ this.shipsSunk++;
+ }
+ return true;
+ }
+ }
+ return false;
+ }
+isSunk: function(ship) {
+ for (var i = 0; i < this.shipLength; i++) {
+ if (ship.hits[i] !== "hit") {
+ return false;
+ }
+ }
+ return true;
+}
+
 };
 
 // view.displayMiss("11");
