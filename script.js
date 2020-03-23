0501d5ccfa2e3385359854751ac2a74e95ab1ab1
Livanenkova <arina909@yandex.ru>
2020-03-23 22:38:39 +0300

    add init

--- script.js
+++ script.js
@@ -88,18 +88,28 @@ var controller = {
  }
 };
 
+function init() {
+ var fireButton = document.getElementById("fireButton");
+ fireButton.onclick = handleFireButton;
+}
+function handleFireButton() {
+ // Код получения данных от формы
+}
+window.onload = init;
 
 
-controller.processGuess("A0");
-controller.processGuess("A6");
-controller.processGuess("B6");
-controller.processGuess("C6");
-controller.processGuess("C4");
-controller.processGuess("D4");
-controller.processGuess("E4");
-controller.processGuess("B0");
-controller.processGuess("B1");
-controller.processGuess("B2");
+
+
+// controller.processGuess("A0");
+// controller.processGuess("A6");
+// controller.processGuess("B6");
+// controller.processGuess("C6");
+// controller.processGuess("C4");
+// controller.processGuess("D4");
+// controller.processGuess("E4");
+// controller.processGuess("B0");
+// controller.processGuess("B1");
+// controller.processGuess("B2");
 
 
 // console.log(parseGuess("A0"));
