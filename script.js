0034a4633cc0eeba82e4105b8f740019526bd83c
Livanenkova <arina909@yandex.ru>
2020-03-24 15:54:55 +0300

    add

--- script.js
+++ script.js
@@ -91,10 +91,24 @@ var controller = {
 function init() {
  var fireButton = document.getElementById("fireButton");
  fireButton.onclick = handleFireButton;
+  var guessInput = document.getElementById("guessInput");
+ guessInput.onkeypress = handleKeyPress;
 }
 function handleFireButton() {
- // Код получения данных от формы
+ var guessInput = document.getElementById("guessInput");
+ var guess = guessInput.value;
+ controller.processGuess(guess);
+ guessInput.value = "";
 }
+
+function handleKeyPress(e) {
+ var fireButton = document.getElementById("fireButton");
+ if (e.keyCode === 13) {
+ fireButton.click();
+ return false;
+ }
+}
+
 window.onload = init;
 
 
